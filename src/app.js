c25485872e0e4280e032592817c6fc35845b407b
murniaw <2225230020@untirta.ac.id>
2024-05-19 00:06:57 +0700

    update fitur belanja + replace info product

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
     items: [
-      { id: 1, name: "Bun", img: "menu1.jpg", price: 7000 },
-      { id: 2, name: "cake", img: "menu2.jpg", price: 19000 },
-      { id: 3, name: "cwaffle", img: "menu1.jpg", price: 15000 },
+      { id: 1, name: "Moon  Pizza Rolls", img: "menu3.jpg", price: 10000 },
+      { id: 2, name: "Moon Ppang", img: "menu4.png", price: 8000 },
+      { id: 3, name: "Moon Cupcake", img: "menu5.jpg", price: 17000 },
+      { id: 4, name: "Moon Croissant", img: "menu6.png", price: 15000 },
+      { id: 5, name: "Moon Shortcake", img: "menu7.jpg", price: 20000 },
+      { id: 6, name: "Banana Puff", img: "menu8.png", price: 7000 },
     ],
   }));
   Alpine.store("cart", {
@@ -62,6 +65,47 @@ document.addEventListener("alpine:init", () => {
     },
   });
 });
+
+//Form validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+//kirim data checkout diklik
+checkoutButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  window.open("http://wa.me/6281284557237?text=" + encodeURIComponent(message));
+});
+
+//format pesan wa
+const formatMessage = (obj) => {
+  return `Data Customer
+  Nama: ${obj.name}
+  Email: ${obj.email}
+  No HP: ${obj.phone}
+  Data Pesanan
+  ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)})\n`)}
+  TOTAL: ${rupiah(obj.total)}
+  Terima Kasih`;
+};
 
 //Rupiah
 const rupiah = (number) => {
